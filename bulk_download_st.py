2f6e5f220ae8ac5399b283479ca4a33dc993aecb
KSHHutch <60274977+KSHHutch@users.noreply.github.com>
2024-02-05 20:27:56 -0500

    Update bulk_download_st.py
    Major changes, commit before changing downloader to use pd instead of csv

--- bulk_download_st.py
+++ bulk_download_st.py
@@ -2,45 +2,35 @@ import streamlit as st
 import pandas as pd
 from datetime import date
 import os
+import csv
 import getpass
-
-st.write("Bulk Downloader v4")
-
-# Open CSV
-uploaded_file = st.file_uploader(label="Choose a file:", type=["csv"])
-path = ""
-column_names = []
-
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    column_names = list(df.columns)
-
-st.write(f"{path}")
+import requests
+import re
+import numpy as np
 
 
-# Select URL Column
-link_column = st.selectbox("Which column has the URL?", column_names)
+# Functions
+def save_image(path, file_name_columns, row, url):
+    """Downloads images from the url to the path.  file_name_columns specifies the name of the downloaded file."""
+    # Make unique file name
+    name = check_unique(path, file_name_columns, row, url)
 
-# Output name and location
-today = str(date.today())
-folder_name = st.text_input(
-    label="Output Folder Name:", value=f"{today}-{uploaded_file.file.name}"
-)
+    # Save file
+    file_name = f"{path}/{name}"
+    img_data = requests.get(url).content
+    with open(f"{file_name}", "wb") as handler:
+        handler.write(img_data)
 
 
-# Select File Name Columns
-
-
-# Ensure unique file names
-def check_unique(path, file_name_widget, row, url):
+def check_unique(path, file_name_columns, row, url):
     """Ensure Unique File Names"""
-    name = "_".join([row[x] for x in file_name_widget.value]) + "." + url.split(".")[-1]
+    name = "_".join([row[x] for x in file_name_columns]) + "." + url.split(".")[-1]
     file_list = os.listdir(path)
     matches = [name == x for x in file_list]
     i = 1
     while matches.count(True) != 0:
         name = (
-            "_".join([row[x] for x in file_name_widget.value])
+            "_".join([row[x] for x in file_name_columns])
             + f"_{i}"
             + "."
             + url.split(".")[-1]
@@ -50,27 +40,59 @@ def check_unique(path, file_name_widget, row, url):
     return name
 
 
-def make_output_folder(folder_name=str(date.today())):
+def bulk_download(link_column, csv_name, file_name_columns, path):
+    # Make output folder
     try:
-        # Make new folder
-        user_name = getpass.getuser()
-        name = folder_name
-        path = f"Users/{user_name}/Downloads/{name}"
         os.mkdir(path)
     except:
         pass
 
-
-def bulk_download(link_column, uploaded_file):
     # Get URL Columns from Widgets
-    url_column = link_column
-    csv_name = uploaded_file.name
+    link_column = link_column
+    csv_name = csv_name
 
     # Download if url is present
     with open(csv_name, newline="") as csvfile:
         read_csv = csv.DictReader(csvfile, delimiter=",")
         for row in read_csv:
-            if row[url_column] == "":
+            if row[link_column] == "" or row[link_column] == np.nan:
                 pass
             else:
-                save_image(path, file_name_box, row, row[url_column])
+                save_image(path, file_name_columns, row, row[link_column])
+
+
+st.write("Bulk Downloader v4")
+
+# Open CSV
+uploaded_file = st.file_uploader(label="Choose a file:", type=["csv"])
+column_names = []
+csv_name = ""
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    column_names = list(df.columns)
+    csv_name = re.split("[\.]", uploaded_file.name)[0]
+
+st.write(f"{csv_name}")
+
+# Select URL Column
+link_column = st.selectbox("Which column has the download link?", column_names)
+
+# Output name and location
+today = str(date.today())
+folder_name = st.text_input(label="Output Folder Name:", value=f"{today}-{csv_name}")
+user_name = getpass.getuser()
+path = f"/Users/{user_name}/Downloads/{folder_name}"
+
+# Select File Name Columns
+file_name_columns = st.multiselect(
+    "What information you want in the downloaded file names?", column_names
+)
+
+# Run Downloader Button
+if st.button("Run Downloader"):
+    bulk_download(link_column, csv_name, file_name_columns, path)
+
+
+## TODO
+# Rejigger downloader so it draws on the dataframe made from the CSV to download instead of the raw CSV
